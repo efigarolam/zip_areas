1b49203dd1ccb75974a74952d9739ac3c2dc6ab4
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-01-18 11:30:29 -0600

    Adding the root to the demo#index controller#action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ZipAreas::Application.routes.draw do
-  get "demo/index"
+  root 'demo#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
