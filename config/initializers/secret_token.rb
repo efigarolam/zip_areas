f2ebeaa2014b6d8568bda59c9ba9d0090b5e7438
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-01-18 11:12:19 -0600

    Initial commit for a new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZipAreas::Application.config.secret_key_base = '4df3b7640651be64a351a4b46308eef3027e099b12fad5624d758b823a0e856f27742eb82116b82314f1b5b71a2abc9cef3e39dc0c4bfa1ccb23982c100c8118'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZipAreas::Application.config.session_store :cookie_store, key: '_zip_areas_session'
