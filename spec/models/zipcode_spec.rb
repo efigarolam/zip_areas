3026cc3276e6b9e0139ecb7d8e6ca49fb9cb6f94
Juan Carlos <zalapeach@gmail.com>
2014-04-02 13:53:35 -0600

    added spec for dependent destroy at zipcode model

--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -5,6 +5,7 @@ describe Zipcode do
 
   it { expect(zipcode).to respond_to(:name) }
   it { expect(zipcode).to have_many(:coordinates) }
+  it { expect(zipcode).to have_many(:coordinates).dependent(:destroy) }
   it { expect(zipcode).to be_valid }
 
   describe "name validations" do
