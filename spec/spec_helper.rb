e291671ef103bbc94982067ffa0255c245b203cd
Juan Carlos <zalapeach@gmail.com>
2014-04-02 12:08:43 -0600

    configured database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,21 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
 
   config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clear
+  end
 end
