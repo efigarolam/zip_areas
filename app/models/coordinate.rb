87b003099bb0ae3de97038826b36ab5c7851fce2
Juan Carlos <zalapeach@gmail.com>
2014-04-02 17:14:31 -0600

    passing specs

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,14 +1,8 @@
 class Coordinate < ActiveRecord::Base
   belongs_to :zipcode
 
-  validates :longitude,
-    presence: true,
-    length: { in: 8..10 },
-    numericality: true
-  validates :latitude,
-    presence: true,
-    length: { in: 8..10 },
-    numericality: true
-  validates :zipcode_id,
-    presence: true
+  validates :longitude, presence: true, numericality: true
+  validates :latitude, presence: true, numericality: true
+  validates :zipcode_id, presence: true
 end
+
--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -3,3 +3,4 @@ class Zipcode < ActiveRecord::Base
 
   validates :name, presence: true
 end
+
