43a54e3d0c510edcd920ec225c70a21a47ecb5bc
Juan Carlos <zalapeach@gmail.com>
2014-04-07 17:26:56 -0500

    refactor zipcodes controller specs

--- spec/controllers/api/v1/zipcodes_controller_spec.rb
+++ spec/controllers/api/v1/zipcodes_controller_spec.rb
@@ -5,110 +5,111 @@ describe Api::V1::ZipcodesController do
   let!(:zipcode2) { create(:zipcode) }
   let(:new_zipcode) { attributes_for(:zipcode) }
 
+  shared_examples_for "response" do
+    it { expect(response.content_type).to eq "application/json" }
+    it { expect(response.status).to eq http_code }
+    it { expect(response.body).to eq response_body }
+  end
+
   describe "#index" do
+    let(:http_code) { 200 }
+    let(:response_body) { Zipcode.all.to_json }
+
     before { get :index }
 
-    it { expect(response.status).to eq 200 }
-    it { expect(response.content_type).to eq "application/json" }
-    it "returns all of zipcodes" do
-      zipcodes = JSON.parse(response.body, symbolize_names: true)
-      expect(zipcodes.count).to eq Zipcode.count
-    end
+    it_should_behave_like "response"
   end
 
   describe "#show" do
     context "the record exists" do
+      let(:http_code) { 200 }
+      let(:response_body) { zipcode1.to_json }
+
       before { get :show, id: zipcode1.id }
 
-      it { expect(response.status).to eq 200 }
-      it { expect(response.content_type).to eq "application/json" }
-      it "returns zipcode1" do
-        zipcode = JSON.parse(response.body, symbolize_names: true)
-        expect(zipcode[:id]).to eq zipcode1.id
-      end
+      it_should_behave_like "response"
     end
 
     context "record doesn't exist" do
+      let(:http_code) { 404 }
+      let(:response_body) do
+        { error: "Sorry, but this record doesn't exist" }.to_json
+      end
+
       before { get :show, id: "xxx" }
 
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 404 }
-      it "returns error message" do
-        json = JSON.parse(response.body, symbolize_names: true)
-        expect(json[:error]).to eq "Sorry, but this record doesn't exist"
-      end
+      it_should_behave_like "response"
     end
   end
 
   describe "#create" do
     context "valid data" do
+      let(:http_code) { 204 }
+      let(:response_body) { "" }
+
       before { post :create , zipcode: new_zipcode, format: :json }
 
+      it_should_behave_like "response"
       it { expect(Zipcode.count).to eq 3 }
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.body).to eq "" }
-      it { expect(response.status).to eq 204 }
     end
 
     context "invalid data" do
+      let(:http_code) { 422 }
+      let(:response_body) { { name: ["can't be blank"] }.to_json }
+
       before do
         new_zipcode[:name] = ""
         post :create, zipcode: new_zipcode, format: :json
       end
 
+      it_should_behave_like "response"
       it { expect(Zipcode.count).to eq 2 }
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 422 }
-      it "returns a json with the errors" do
-        json = JSON.parse(response.body, symbolize_names: true)
-        expect(json[:name]).to eq ["can't be blank"]
-      end
     end
   end
 
   describe "#update" do
     context "valid data" do
+      let(:http_code) { 204 }
+      let(:response_body) { "" }
+
       before { patch :update, id: zipcode1, zipcode: new_zipcode }
 
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 204 }
-      it { expect(response.body).to eq "" }
+      it_should_behave_like "response"
     end
 
     context "invalid data" do
+      let(:http_code) { 422 }
+      let(:response_body) { { name: ["can't be blank"] }.to_json }
+
       before do
         new_zipcode[:name] = ""
         post :create, zipcode: new_zipcode, format: :json
       end
 
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 422 }
-      it "returns a json with errors" do
-        json = JSON.parse(response.body, symbolize_names: true)
-        expect(json[:name]).to eq ["can't be blank"]
-      end
+      it_should_behave_like "response"
     end
   end
 
   describe "#destroy" do
     context "record exits" do
+      let(:http_code) { 204 }
+      let(:response_body) { "" }
+
       before { delete :destroy, id: zipcode1 }
 
+      it_should_behave_like "response"
       it { expect(Zipcode.count).to eq 1 }
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 204 }
-      it { expect(response.body).to eq "" }
     end
 
     context "record doesn't exist" do
+      let(:http_code) { 404 }
+      let(:response_body) do
+        { error: "Sorry, but this record doesn't exist" }.to_json
+      end
+
       before { delete :destroy, id: "xxx" }
 
-      it { expect(response.content_type).to eq "application/json" }
-      it { expect(response.status).to eq 404 }
-      it "returns error message" do
-        json = JSON.parse(response.body, symbolize_names: true)
-        expect(json[:error]).to eq "Sorry, but this record doesn't exist"
-      end
+      it_should_behave_like "response"
     end
   end
 end
