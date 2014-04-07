fc7257b6f812f7fe689e65c1c768eba6da8ebeb4
Juan Carlos <zalapeach@gmail.com>
2014-04-07 14:01:59 -0500

    passing specs for coordinates controller

--- app/controllers/api/v1/coordinates_controller.rb
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -1,37 +1,47 @@
-class Api::V1::CoordinatesController < Api::V1::BaseController
-  expose(:coordinate, attributes: :coordinate_params)
-  expose(:coordinates)
+module Api
+  module V1
+    class CoordinatesController < Api::V1::BaseController
+      expose(:coordinate, attributes: :coordinate_params)
+      expose(:coordinates)
 
-  def index
-    respond_to do |format|
-      format.json { render json: coordinates, status: 200 }
-      format.xml { render json: coordinates, status: 200 }
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+      def index
+        render json: coordinates, status: 200
+      end
+
+      def show
+        render json: coordinate, status: 200
+      end
+
+      def create
+        if coordinate.save
+          render json: "", status: 204
+        else
+          render json: coordinate.errors, status: 422
+        end
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      private
+
+      def coordinate_params
+        params.require(:coordinate).permit(
+          :longitude,
+          :latitude,
+          :zipcode_id
+        )
+      end
+
+      def record_not_found
+        render json: { error: "Sorry, but this record doesn't exist"},
+          status: 404
+      end
     end
-  end
-
-  def show
-    respond_to do |format|
-      format.json { render json: coordinate, status: 200 }
-      format.xml { render json: coordinate, status: 200 }
-    end
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def coordinate_params
-    params(:coordinate).permit(
-      :longitude,
-      :latitude,
-      :zipcode
-    )
   end
 end
