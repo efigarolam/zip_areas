951ee05f12bfa4d0d4c1a0978c475f3ea73db897
Juan Carlos <zalapeach@gmail.com>
2014-04-08 18:51:09 -0500

    deleted unnecessary tests

--- spec/controllers/api/v1/coordinates_controller_spec.rb
+++ spec/controllers/api/v1/coordinates_controller_spec.rb
@@ -1,114 +1,39 @@
-require "spec_helper"
-require "support/shared_examples"
+require 'spec_helper'
+require 'support/shared_examples'
 
 describe Api::V1::CoordinatesController do
   let!(:coordinate1) { create(:coordinate) }
   let!(:coordinate2) { create(:coordinate) }
   let(:new_coordinate) { attributes_for(:coordinate) }
 
-  describe "#index" do
+  describe '#index' do
     let(:http_code) { 200 }
     let(:response_body) { Coordinate.all.to_json }
 
     before { get :index }
 
-    it_should_behave_like "response"
+    it_should_behave_like 'response'
   end
 
-  describe "#show" do
-    context "the record exists" do
+  describe '#show' do
+    context 'when the record exists' do
       let(:http_code) { 200 }
       let(:response_body) { coordinate1.to_json }
 
       before { get :show, id: coordinate1 }
 
-      it_should_behave_like "response"
+      it_should_behave_like 'response'
     end
 
-    context "record doesn't exist" do
+    context "when the record doesn't exist" do
       let(:http_code) { 404 }
       let(:response_body) do
         { error: "Sorry, but this record doesn't exist" }.to_json
       end
 
-      before { get :show, id: "xxx" }
+      before { get :show, id: 'xxx' }
 
-      it_should_behave_like "response"
-    end
-  end
-
-  describe "#create" do
-    context "valid data" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { post :create , coordinate: new_coordinate, format: :json }
-
-      it_should_behave_like "response"
-      it { expect(Coordinate.count).to eq 3 }
-    end
-
-    context "invalid data" do
-      let(:http_code) { 422 }
-      let(:response_body) do
-        { longitude: ["can't be blank", "is not a number"] }.to_json
-      end
-
-      before do
-        new_coordinate[:longitude] = ""
-        post :create, coordinate: new_coordinate, format: :json
-      end
-
-      it_should_behave_like "response"
-      it { expect(Coordinate.count).to eq 2 }
-    end
-  end
-
-  describe "#update" do
-    context "valid data" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { patch :update, id: coordinate1, coordinate: new_coordinate }
-
-      it_should_behave_like "response"
-    end
-
-    context "invalid data" do
-      let(:http_code) { 422 }
-      let(:response_body) do
-        { longitude: ["can't be blank", "is not a number"] }.to_json
-      end
-
-      before do
-        new_coordinate[:longitude] = ""
-        post :create, coordinate: new_coordinate, format: :json
-      end
-
-      it_should_behave_like "response"
-    end
-  end
-
-  describe "#destroy" do
-    context "record exits" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { delete :destroy, id: coordinate1 }
-
-      it_should_behave_like "response"
-      it { expect(Coordinate.count).to eq 1 }
-    end
-
-    context "record doesn't exist" do
-      let(:http_code) { 404 }
-      let(:response_body) do
-        { error: "Sorry, but this record doesn't exist" }.to_json
-      end
-
-      before { delete :destroy, id: "xxx" }
-
-      it_should_behave_like "response"
+      it_should_behave_like 'response'
     end
   end
 end
--- spec/controllers/api/v1/zipcodes_controller_spec.rb
+++ spec/controllers/api/v1/zipcodes_controller_spec.rb
@@ -1,110 +1,39 @@
-require "spec_helper"
-require "support/shared_examples"
+require 'spec_helper'
+require 'support/shared_examples'
 
 describe Api::V1::ZipcodesController do
   let!(:zipcode1) { create(:zipcode) }
   let!(:zipcode2) { create(:zipcode) }
   let(:new_zipcode) { attributes_for(:zipcode) }
 
-  describe "#index" do
+  describe '#index' do
     let(:http_code) { 200 }
     let(:response_body) { Zipcode.all.to_json }
 
     before { get :index }
 
-    it_should_behave_like "response"
+    it_should_behave_like 'response'
   end
 
-  describe "#show" do
-    context "the record exists" do
+  describe '#show' do
+    context 'when the record exists' do
       let(:http_code) { 200 }
       let(:response_body) { zipcode1.to_json }
 
       before { get :show, id: zipcode1.id }
 
-      it_should_behave_like "response"
+      it_should_behave_like 'response'
     end
 
-    context "record doesn't exist" do
+    context "when the record doesn't exist" do
       let(:http_code) { 404 }
       let(:response_body) do
         { error: "Sorry, but this record doesn't exist" }.to_json
       end
 
-      before { get :show, id: "xxx" }
+      before { get :show, id: 'xxx' }
 
-      it_should_behave_like "response"
-    end
-  end
-
-  describe "#create" do
-    context "valid data" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { post :create , zipcode: new_zipcode, format: :json }
-
-      it_should_behave_like "response"
-      it { expect(Zipcode.count).to eq 3 }
-    end
-
-    context "invalid data" do
-      let(:http_code) { 422 }
-      let(:response_body) { { name: ["can't be blank"] }.to_json }
-
-      before do
-        new_zipcode[:name] = ""
-        post :create, zipcode: new_zipcode, format: :json
-      end
-
-      it_should_behave_like "response"
-      it { expect(Zipcode.count).to eq 2 }
-    end
-  end
-
-  describe "#update" do
-    context "valid data" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { patch :update, id: zipcode1, zipcode: new_zipcode }
-
-      it_should_behave_like "response"
-    end
-
-    context "invalid data" do
-      let(:http_code) { 422 }
-      let(:response_body) { { name: ["can't be blank"] }.to_json }
-
-      before do
-        new_zipcode[:name] = ""
-        post :create, zipcode: new_zipcode, format: :json
-      end
-
-      it_should_behave_like "response"
-    end
-  end
-
-  describe "#destroy" do
-    context "record exits" do
-      let(:http_code) { 204 }
-      let(:response_body) { "" }
-
-      before { delete :destroy, id: zipcode1 }
-
-      it_should_behave_like "response"
-      it { expect(Zipcode.count).to eq 1 }
-    end
-
-    context "record doesn't exist" do
-      let(:http_code) { 404 }
-      let(:response_body) do
-        { error: "Sorry, but this record doesn't exist" }.to_json
-      end
-
-      before { delete :destroy, id: "xxx" }
-
-      it_should_behave_like "response"
+      it_should_behave_like 'response'
     end
   end
 end
