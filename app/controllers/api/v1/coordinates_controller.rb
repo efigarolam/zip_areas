2e572c4a737a11ad07ea26447fb9811294521959
Juan Carlos <zalapeach@gmail.com>
2014-04-08 18:51:52 -0500

    passing specs without decent exposure

--- app/controllers/api/v1/coordinates_controller.rb
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -1,37 +1,12 @@
 module Api
   module V1
     class CoordinatesController < Api::V1::BaseController
-      expose(:coordinate, attributes: :coordinate_params)
-      expose(:coordinates)
-
       def index
         render json: coordinates, status: 200
       end
 
       def show
         render json: coordinate, status: 200
-      end
-
-      def create
-        if coordinate.save
-          render json: '', status: 204
-        else
-          render json: coordinate.errors, status: 422
-        end
-      end
-
-      def update
-        if coordinate.update(coordinate_params)
-          render json: '', status: 204
-        else
-          render json: coordinate.errors, status: 422
-        end
-      end
-
-      def destroy
-        if coordinate.destroy
-          render json: '', status: 204
-        end
       end
 
       private
@@ -42,6 +17,14 @@ module Api
           :latitude,
           :zipcode_id
         )
+      end
+
+      def coordinate
+        @coordinate ||= Coordinate.find(params[:id])
+      end
+
+      def coordinates
+        @coordinates ||= Coordinate.all
       end
     end
   end
--- app/controllers/api/v1/zipcodes_controller.rb
+++ app/controllers/api/v1/zipcodes_controller.rb
@@ -1,10 +1,6 @@
 module Api
   module V1
     class ZipcodesController < Api::V1::BaseController
-      expose(:zipcode, attributes: :zipcode_params)
-      expose(:new_zipcode) { zipcodes.build(zipcode_params) }
-      expose(:zipcodes)
-
       def index
         render json: zipcodes, status: 200
       end
@@ -13,34 +9,20 @@ module Api
         render json: zipcode, status: 200
       end
 
-      def create
-        if new_zipcode.save
-          render json: '', status: 204
-        else
-          render json: new_zipcode.errors, status: 422
-        end
-      end
-
-      def update
-        if zipcode.update(zipcode_params)
-          render json: '', status: 204
-        else
-          render json: zipcode.errors, status: 422
-        end
-      end
-
-      def destroy
-        if zipcode.destroy
-          render json: '', status: 204
-        end
-      end
-
       private
 
       def zipcode_params
         params.require(:zipcode).permit(
           :name
         )
+      end
+
+      def zipcode
+        @zipcode ||= Zipcode.find(params[:id])
+      end
+
+      def zipcodes
+        @zipcodes ||= Zipcode.all
       end
     end
   end
